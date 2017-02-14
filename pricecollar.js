4a3cab6832f061579280ea3ac5ef029ab7f2eaac
mckennah <mck2151882@maricopa.edu>
2017-02-13 18:37:16 -0700

    last maybe

--- pricecollar.js
+++ pricecollar.js
@@ -1,6 +1,8 @@
 //to calculate the cost of a collar selected, along with any add-ons
 	function calcTotal() {
 	var orderTotal = 0;
+	var orderTotalRounded = 0;
+	//defining those as zero first
 	var size1 = document.getElementById("size1"); //size S
 	var size2 = document.getElementById("size2"); //size M
 	var size3 = document.getElementById("size3"); //size L
@@ -17,7 +19,7 @@
 	var orderTotal = orderTotal + (orderTotal * tempeTaxRate); 
 //this is so I don't get the weird decimal issues. The example in the book would not show a 0 in the cents ex. 22.10 was 22.1
 //to prove there was an issue with this try doing a large collar with gold hardware. I think that's the only instance
-	var orderTotalRounded = (Math.round(orderTotal*Math.pow(10,2))/Math.pow(10,2)).toFixed(2);
+	var orderTotalRounded = (Math.round(orderTotal*(100))/(100)).toFixed(2);//turns it from decimal number to integer and back after rounding
 	
 	alert("Your custom order total is $" + orderTotalRounded + "!");//order total is rounded because only two decimals allowed
 	}
